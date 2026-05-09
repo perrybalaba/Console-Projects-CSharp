527aa194418c8cc31a0af40b9de6dd8f6adf619b
Perry Balaba <balabaperry@gmail.com>
2026-05-08 22:03:57 -0400

    added gym member class

---
+++ Gym-Membership-Tracker/GymMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Membership_Tracker
+{
+    public enum MembershipType
+    {
+        Basic,
+        Premium,
+        VIP
+    }
+
+    public class GymMember
+    {
+    }
+}
